f5240fb799ee374b97fb75b36b0cf3e1c90b18ed
Jansen Pimentel <jap264@njit.edu>
2021-03-09 23:42:11 -0500

    player can move and rotates based on the mouse

--- src/game.c
+++ src/game.c
@@ -4,6 +4,8 @@
 #include "simple_logger.h"
 
 #include "player.h"
+#include "shape.h"
+#include "camera.h"
 #include "entity.h"
 
 int main(int argc, char * argv[])
@@ -33,6 +35,8 @@ int main(int argc, char * argv[])
         vector4d(0,0,0,255),
         0);
     gf2d_graphics_set_frame_delay(16);
+	camera_set_dimensions(vector2d(1200, 720));
+	camera_set_position(vector2d(0, 0));
     gf2d_sprite_init(1024);
 	entity_manager_init(1024);
 
@@ -43,7 +47,7 @@ int main(int argc, char * argv[])
     mouse = gf2d_sprite_load_all("images/pointer.png",32,32,16);
 
 	//Player Spawn
-	player_spawn();
+	player_spawn(vector2d(100,100));
 	player = get_player();
 	playerEnt = get_player_entity();
 
@@ -58,15 +62,18 @@ int main(int argc, char * argv[])
         mf+=0.1;
         if (mf >= 16.0)mf = 0;
         
+		entity_manager_think_entities();
 		entity_manager_update_entities();
         
         gf2d_graphics_clear_screen();// clears drawing buffers
         // all drawing should happen betweem clear_screen and next_frame
             //backgrounds drawn first
             gf2d_sprite_draw_image(background,vector2d(0,0));
-			gf2d_sprite_draw_image(player->ent->sprite, vector2d(0, 0));
+			//gf2d_sprite_draw_image(player->ent->sprite, vector2d(0, 0));
 
-            //UI elements last
+			entity_manager_draw_entities();
+            
+			//UI elements last
             gf2d_sprite_draw(
                 mouse,
                 vector2d(mx,my),
--- src/player.c
+++ src/player.c
@@ -1,46 +1,82 @@
 #include "simple_logger.h"
 
-#include "entity.h"
 #include "player.h"
+#include "camera.h"
+#include "entity.h"
 
 void player_update(Entity *self);
+void player_think(Entity *self);
 
 static Player *player = { 0 };
 
-Entity *player_spawn()
+Entity *player_spawn(Vector2D position)
 {
 	player = (Player *)gfc_allocate_array(sizeof(Player), 1);
-	Entity *ent = entity_new();
+	Entity *ent = ent = entity_new();
 	if (!ent)
 	{
 		slog("failed to create entity for the player");
 		return NULL;
 	}
 	player->ent = ent;
-	player->ent->ent_type = 0;
-	player->ent->position = vector2d(0, 0);
-	player->ent->sprite = gf2d_sprite_load_image("images/player.png");
-	//vector2d_copy(player->ent->position, position);
-	/*player->ent->frameRate = 0.1;
-	ent->frameCount = 16;
+	player->ent->sprite = gf2d_sprite_load_all("images/ed210_top.png", 128, 128, 16);
+	vector2d_copy(player->ent->position, position);
+	player->ent->frameRate = 0.1;
+	player->ent->frameCount = 16;
 	player->ent->update = player_update;
+	player->ent->think = player_think;
 	player->ent->rotation.x = 64;
-	player->ent->rotation.y = 64;*/
-	return ent;
+	player->ent->rotation.y = 64;
+	return player->ent;
 }
 
 
 void player_update(Entity *self)
 {
-	Vector2D aimdir;
+	Vector2D camera;
+	Vector2D cameraSize;
+
+	if (!self)return;
+	cameraSize = camera_get_dimensions();
+	camera.x = (self->position.x + 64) - (cameraSize.x * 0.5);
+	camera.y = (self->position.y + 64) - (cameraSize.y * 0.5);
+	camera_set_position(camera);
+	// apply dampening on velocity
+	vector2d_scale(self->velocity, self->velocity, 0.75);
+	if (vector2d_magnitude_squared(self->velocity) < 2)
+	{
+		vector2d_clear(self->velocity);
+	}
+}
+
+void player_think(Entity *self)
+{
+	const Uint8 *keys;
+	Vector2D aimdir, camera, thrust;
 	float angle;
 	int mx, my;
 	if (!self)return;
+	keys = SDL_GetKeyboardState(NULL);
 	SDL_GetMouseState(&mx, &my);
+	camera = camera_get_position();
+	mx += camera.x;
+	my += camera.y;
 	aimdir.x = mx - (self->position.x + 64);
 	aimdir.y = my - (self->position.y + 64);
 	angle = vector_angle(aimdir.x, aimdir.y);
 	self->rotation.z = angle + 90;
+
+	// turn aimdir into a unit vector
+	vector2d_normalize(&aimdir);
+	// check for motion
+	if (keys[SDL_SCANCODE_W])
+	{
+		vector2d_scale(thrust, aimdir, 5);
+		vector2d_add(self->velocity, self->velocity, thrust);
+	}
+
+
+
 }
 
 Entity *get_player_entity()
